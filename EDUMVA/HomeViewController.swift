eef471f6c5925a5a2426912d882e40e76b56d5f4
EDUMVA2 <edumva2@adsl-172-14-0-52.dsl.lgtpmi.sbcglobal.net>
2018-09-05 15:09:04 +0530

    Name Changed By Archit

---
+++ EDUMVA/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EDUMVA
+//
+//  Created by EDUMVA2 on 05/09/18.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
