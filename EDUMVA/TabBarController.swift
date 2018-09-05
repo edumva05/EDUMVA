119e81080c7910de7ee785090e75ffcd7acf957b
EDUMVA5 <edumva5@mva53s-imac.local>
2018-09-05 19:01:08 +0530

    new

---
+++ EDUMVA/TabBarController.swift
@@ -0,0 +1,65 @@
+//
+//  TabBarController.swift
+//  EDUMVA
+//
+//  Created by EDUMVA5 on 05/09/18.
+//
+
+import UIKit
+
+class TabBarController: UIViewController {
+    @IBOutlet weak var tabView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabView.layer.applySketchShadow(
+            color: .black,
+            alpha: 0.7,
+            x: 0,
+            y: 0,
+            blur: 16,
+            spread: 0)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension CALayer {
+    func applySketchShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0)
+    {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+}
