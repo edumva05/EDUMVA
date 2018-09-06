899ae93cf6b6b2617b9588b5423cba3441588d3d
EDUMVA2 <edumva2@adsl-172-14-0-52.dsl.lgtpmi.sbcglobal.net>
2018-09-06 16:07:39 +0530

    LAtest push for edumva

--- EDUMVA/TabBarController.swift
+++ EDUMVA/TabBarController.swift
@@ -7,18 +7,52 @@
 
 import UIKit
 
-class TabBarController: UIViewController {
+class TabBarController: UIViewController, UIGestureRecognizerDelegate  {
     @IBOutlet weak var tabView: UIView!
+    
+    var viewControllers: [UIViewController]!
+    
+    var FeedsViewController: UIViewController!
+    var ExamViewController: UIViewController!
+    var HomeViewController: UIViewController!
+    var FtpViewController: UIViewController!
+    var ProflieViewController: UIViewController!
+    
+    var selectedIndex: Int = 2
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    @IBOutlet weak var viewDisplay: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabView.layer.applySketchShadow(
+      /*  tabView.layer.applySketchShadow(
             color: .black,
             alpha: 0.7,
             x: 0,
             y: 0,
             blur: 16,
             spread: 0)
+ */
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+        //Initalize storyboards
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        FeedsViewController = storyboard.instantiateViewController(withIdentifier: "FeedsViewController")
+        ExamViewController = storyboard.instantiateViewController(withIdentifier: "ExamViewController")
+        HomeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
+        FtpViewController = storyboard.instantiateViewController(withIdentifier: "FtpViewController")
+        ProflieViewController = storyboard.instantiateViewController(withIdentifier: "ProflieViewController")
+        
+        //Add stroyboards to array
+        viewControllers = [ FeedsViewController, ExamViewController,HomeViewController, FtpViewController, ProflieViewController]
+        
+        //Initallly select a ViewController
+        didPressTab(buttons[selectedIndex])
 
         // Do any additional setup after loading the view.
     }
@@ -29,15 +63,41 @@ class TabBarController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func didPressTab(_ sender: UIButton) {
+        
+        
+        
+        
+        //Keeps track of the previous button
+        let previousIndex = selectedIndex
+        //Set the selectedIndex to the tag value of which ever button was tapped.
+        selectedIndex = sender.tag
+        //Set previous selected button to non-selected state
+        buttons[previousIndex].isSelected = false
+        
+        //Use the previousIndex to access the previous ViewController from the viewControllers array.
+        let previousVC = viewControllers[previousIndex]
+        
+        //Remove the previous ViewController
+        previousVC.willMove(toParentViewController: nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        
+        //Access the selected button and set it to the selected state.
+        sender.isSelected = true
+        
+        //Use the selectedIndex to access the current ViewController from the viewControllers array.
+        let vc = viewControllers[selectedIndex]
+        //Add the new ViewController to display view
+        addChildViewController(vc)
+        
+        //Adjusts the size of the ViewController view add it as a subView of the contentView.
+        vc.view.frame = viewDisplay.bounds
+        viewDisplay.addSubview(vc.view)
+        //Call the viewDidAppear method of the coming ViewController
+        vc.didMove(toParentViewController: self)
+        
     }
-    */
 
 }
 
